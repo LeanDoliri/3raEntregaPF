f7885492399f545f28210eb0f0f6e39f2125b368
Leandro Doliri <leandoliri@gmail.com>
2023-03-09 16:54:35 -0300

    vista info

--- src/routers/web/home.js
+++ src/routers/web/home.js
@@ -4,25 +4,25 @@ const homeWebRouter = new Router();
 
 homeWebRouter.get("/home", async (req, res) => {
   if (!req.session.passport?.user) {
-    res.render("login.ejs");
+    res.render("auth/login.ejs");
   } else {
-    res.render("home.ejs", { nombre: req.session.passport?.user.nombre });
+    res.render("home.ejs", { user: req.session.passport?.user });
   }
 });
 
 homeWebRouter.get("/carrito", async (req, res) => {
   if (!req.session.passport?.user) {
-    res.render("login.ejs");
+    res.render("auth/login.ejs");
   } else {
-    res.render("carrito.ejs", { nombre: req.session.passport?.user.nombre });
+    res.render("carrito.ejs", { user: req.session.passport?.user});
   }
 });
 
 homeWebRouter.get("/mi-perfil", async (req, res) => {
   if (!req.session.passport?.user) {
-    res.render("login.ejs");
+    res.render("auth/login.ejs");
   } else {
-    res.render("mi-perfil.ejs", { nombre: req.session.passport?.user.nombre });
+    res.render("mi-perfil.ejs", { user: req.session.passport?.user });
   }
 });
 
